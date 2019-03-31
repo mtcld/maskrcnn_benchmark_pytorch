c3592d97f0708ca59c744ed8092ebbc79d09e8da
Sulabh Shrestha <tsulabh4@gmail.com>
2019-03-31 11:39:29 +0545

    - Add print statements to check the loss_dict

--- maskrcnn_benchmark/engine/trainer.py
+++ maskrcnn_benchmark/engine/trainer.py
@@ -65,11 +65,25 @@ def do_train(
 
         loss_dict = model(images, targets)
 
+        print('loss_dict======================================')
+        print(loss_dict)
+
         losses = sum(loss for loss in loss_dict.values())
+
+        print('losses ========================================')
+        print(losses)
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = reduce_loss_dict(loss_dict)
+
+        print('loss_dict_reduced =============================')
+        print(loss_dict_reduced)
+
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        print('losses_reduced =================================')
+        print(losses_reduced)
+
         meters.update(loss=losses_reduced, **loss_dict_reduced)
 
         optimizer.zero_grad()
